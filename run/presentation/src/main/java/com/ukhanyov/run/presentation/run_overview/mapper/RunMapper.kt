b8caf6361ac6baee88a0871b1d2aece28e4d0ed1
Oleg <ukhanyov.oleg@gmail.com>
2024-06-19 10:50:10 +0300

    Setting up the Run models done

---
+++ run/presentation/src/main/java/com/ukhanyov/run/presentation/run_overview/mapper/RunMapper.kt
@@ -0,0 +1,29 @@
+package com.ukhanyov.run.presentation.run_overview.mapper
+
+import com.ukhanyov.core.domain.run.Run
+import com.ukhanyov.core.presentation.ui.*
+import com.ukhanyov.run.presentation.run_overview.RunUi
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun Run.toRunUi(): RunUi {
+    val dateTimeInLocalTime = dateTimeUtc
+        .withZoneSameInstant(ZoneId.systemDefault())
+    val formattedDateTime = DateTimeFormatter
+        .ofPattern("MMM dd, yyyy - hh:mma")
+        .format(dateTimeInLocalTime)
+
+    val distanceKm = distanceMeters / 1000.0
+
+    return RunUi(
+        id = id!!,
+        duration = duration.formatted(),
+        dateTime = formattedDateTime,
+        distance = distanceKm.toFormattedKm(),
+        avgSpeed = avgSpeedKmh.toFormattedKmh(),
+        maxSpeed = maxSpeedKmh.toFormattedKmh(),
+        pace = duration.toFormattedPace(distanceKm),
+        totalElevation = totalElevationMeters.toFormattedMeters(),
+        mapPictureUrl = mapPictureUrl
+    )
+}
